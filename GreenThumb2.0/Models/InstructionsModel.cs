d043b5f1b88e210ac0e32901fa71c83b9c531779
oxarious <143609934+oxarious@users.noreply.github.com>
2023-12-04 10:08:52 +0100

    Add project files.

---
+++ GreenThumb2.0/Models/InstructionsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenThumb2._0.Models
+{
+    internal class InstructionsModel
+    {
+        public int Id { get; set; }
+        public string Instructions { get; set; } = null!;
+    }
+}
---
+++ GreenThumb2.0/Models/PlantModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenThumb2._0.Models
+{
+    internal class PlantModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!; 
+        public DateTime PlantDate { get; set; }
+        IEnumerable<InstructorModel> Instructions { get; set; }
+    }
+}
